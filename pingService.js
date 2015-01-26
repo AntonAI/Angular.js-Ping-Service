0ceab95489f2e87cb750b68703c3c71ed554563f
Anton Ivanov <antoni@carttronics.net>
2015-01-26 13:56:33 -0800

    Added pingService.js

---
+++ pingService.js
@@ -0,0 +1,24 @@
+angular.module('pingService', [])
+
+    .factory("ping", function($timeout, $http) {
+
+        // Pings a URL or IP using HTTP GET request
+        var ping = function(URL, callback) {
+
+            var responseTime = 0;
+            var start = (new Date()).getTime();
+
+            $http.get(URL + '?rnd=' + (new Date().getTime()))
+                .success(function() {
+                    responseTime = (new Date().getTime()) - start;
+                    callback(Math.round(responseTime / 10) / 100);
+                })
+                .error(function() {
+                    callback(responseTime);
+                })
+        };
+
+        return {
+            ping: ping
+        };
+    });
