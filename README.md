080635d1c91c0c391ac640b38b93475f20d6cbee
Anton Ivanov <antoni@carttronics.net>
2015-03-30 10:22:57 -0700

    Improved performance, updated README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Angular.js-Ping
-An Angular.js module that pings a specified URL or IP using $http.get.
+An Angular.js module that pings a specified URL or IP using an HTTP GET request.
+## Installation
+- Copy pingService.js to your project's JavaScript directory.
+- Add a dependency for the 'pingService' module to your project.
+- Inject the 'ping' service into your controller(s).
+## Usage
+ping.ping(testURL, callback(response));
+
+If ping was successful, returns the time in took to reach the resource in seconds (ex. 3.24).
+If ping failed, returns 0.
--- pingService.js
+++ pingService.js
@@ -1,25 +1,22 @@
 angular.module('pingService', [])
 
-    .factory("ping", function($timeout, $http) {
+    .factory('ping', ['$http', function($http) {
 
         // Pings a URL or IP using HTTP GET request
-        var ping = function(URL, callback) {
-
-            var responseTime = 0;
-            var start = (new Date()).getTime();
-
-            $http.get(URL + '?rnd=' + (new Date().getTime()))
-                .success(function() {
-                    responseTime = (new Date().getTime()) - start;
-                    callback(Math.round(responseTime / 10) / 100);
-                })
-                .error(function() {
-                    callback(responseTime);
-                })
-        };
-
         return {
-            ping: ping
-        };
-    });
-	
+            ping: function(URL, callback) {
+
+                var responseTime = 0;
+                var start = (new Date()).getTime();
+
+                $http.get(URL + '?rnd=' + (new Date().getTime()))
+                    .success(function() {
+                        responseTime = (new Date().getTime()) - start;
+                        callback(Math.round(responseTime / 10) / 100);
+                    })
+                    .error(function() {
+                        callback(responseTime);
+                    })
+            }
+        }
+    }]);
